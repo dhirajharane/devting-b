1fdee2865c452fdcdc544ed9adc81e3d1288ed1a
dhirajharane <dhirajharane@gmail.com>
2025-10-12 14:58:03 +0530

    fixing the middleware

--- src/middlewears/auth.js
+++ src/middlewears/auth.js
@@ -5,20 +5,20 @@ const userAuth = async (req, res, next) => {
   try {
     const { token } = req.cookies;
     if (!token) {
-      return res.status(401).send("Please login first");  // return added
+      return res.status(401).send("Please login first");
     }
 
     const decodedObj = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findById(decodedObj.id);
 
     if (!user) {
-      return res.status(401).send("Invalid user");  // return added
+      return res.status(401).send("Invalid user");
     }
 
     req.user = user;
     next();
   } catch (err) {
-    return res.status(400).send(err.message); // return added
+    return res.status(400).send(err.message);
   }
 };
 
